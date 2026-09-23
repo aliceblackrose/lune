0ebd3b026830355bb6c7079a4dd409ebb639a362
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-09-23 11:02:36 -0500

    feat: implement Windows child processes

--- bootstrap/platform.c
+++ bootstrap/platform.c
@@ -10,6 +10,7 @@
 #include <string.h>
 
 #if defined(_WIN32)
+#include <io.h>
 #include <windows.h>
 #else
 #include <sys/types.h>
@@ -440,8 +441,6 @@ bool lune_platform_temp_file(
 #endif
 }
 
-#if !defined(_WIN32)
-
 static bool read_capture(
     FILE *file,
     char **data,
@@ -523,6 +522,8 @@ static bool read_capture(
     *length = bytes;
     return true;
 }
+
+#if !defined(_WIN32)
 
 bool lune_platform_exec(
     const char *program,
@@ -696,6 +697,167 @@ bool lune_platform_exec(
 
 #else
 
+static bool windows_command_line(
+    char *const argv[],
+    char **command_line,
+    char *error,
+    size_t error_capacity
+) {
+    size_t capacity = 1;
+
+    for (size_t i = 0; argv[i] != NULL; i++) {
+        size_t length = strlen(argv[i]);
+
+        if (
+            length >
+            (SIZE_MAX - capacity - 3) / 2
+        ) {
+            set_error(
+                error,
+                error_capacity,
+                "process command line is too large"
+            );
+            return false;
+        }
+
+        capacity += length * 2 + 3;
+    }
+
+    char *line = malloc(capacity);
+
+    if (line == NULL) {
+        set_error(
+            error,
+            error_capacity,
+            "out of memory building process command line"
+        );
+        return false;
+    }
+
+    char *out = line;
+
+    for (size_t i = 0; argv[i] != NULL; i++) {
+        const char *argument = argv[i];
+        bool quote =
+            argument[0] == '\0' ||
+            strchr(argument, ' ') != NULL ||
+            strchr(argument, '\t') != NULL ||
+            strchr(argument, '"') != NULL;
+
+        if (i != 0) {
+            *out++ = ' ';
+        }
+
+        if (!quote) {
+            size_t length = strlen(argument);
+            memcpy(out, argument, length);
+            out += length;
+            continue;
+        }
+
+        *out++ = '"';
+        size_t backslashes = 0;
+
+        for (
+            const char *cursor = argument;
+            *cursor != '\0';
+            cursor++
+        ) {
+            if (*cursor == '\\') {
+                backslashes++;
+                continue;
+            }
+
+            if (*cursor == '"') {
+                for (
+                    size_t j = 0;
+                    j < backslashes * 2 + 1;
+                    j++
+                ) {
+                    *out++ = '\\';
+                }
+
+                *out++ = '"';
+                backslashes = 0;
+                continue;
+            }
+
+            for (
+                size_t j = 0;
+                j < backslashes;
+                j++
+            ) {
+                *out++ = '\\';
+            }
+
+            backslashes = 0;
+            *out++ = *cursor;
+        }
+
+        for (
+            size_t j = 0;
+            j < backslashes * 2;
+            j++
+        ) {
+            *out++ = '\\';
+        }
+
+        *out++ = '"';
+    }
+
+    *out = '\0';
+    *command_line = line;
+    return true;
+}
+
+static bool windows_capture_handle(
+    FILE *file,
+    HANDLE *handle,
+    char *error,
+    size_t error_capacity
+) {
+    int fd = _fileno(file);
+
+    if (fd < 0) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to access process capture file"
+        );
+        return false;
+    }
+
+    intptr_t raw = _get_osfhandle(fd);
+
+    if (raw == -1) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to access process capture handle"
+        );
+        return false;
+    }
+
+    HANDLE value = (HANDLE)raw;
+
+    if (!SetHandleInformation(
+        value,
+        HANDLE_FLAG_INHERIT,
+        HANDLE_FLAG_INHERIT
+    )) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to prepare process capture (Win32 error %lu)",
+            (unsigned long)GetLastError()
+        );
+        return false;
+    }
+
+    *handle = value;
+    return true;
+}
+
 bool lune_platform_exec(
     const char *program,
     char *const argv[],
@@ -704,15 +866,209 @@ bool lune_platform_exec(
     size_t error_capacity
 ) {
     (void)program;
-    (void)argv;
     *result = (LuneProcessResult){0};
 
-    set_error(
+    FILE *stdout_file = tmpfile();
+
+    if (stdout_file == NULL) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to create stdout capture: %s",
+            strerror(errno)
+        );
+        return false;
+    }
+
+    FILE *stderr_file = tmpfile();
+
+    if (stderr_file == NULL) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to create stderr capture: %s",
+            strerror(errno)
+        );
+        fclose(stdout_file);
+        return false;
+    }
+
+    char *command_line = NULL;
+
+    if (!windows_command_line(
+        argv,
+        &command_line,
         error,
-        error_capacity,
-        "child processes are not implemented on this platform"
+        error_capacity
+    )) {
+        fclose(stderr_file);
+        fclose(stdout_file);
+        return false;
+    }
+
+    HANDLE stdout_handle = NULL;
+    HANDLE stderr_handle = NULL;
+
+    if (
+        !windows_capture_handle(
+            stdout_file,
+            &stdout_handle,
+            error,
+            error_capacity
+        ) ||
+        !windows_capture_handle(
+            stderr_file,
+            &stderr_handle,
+            error,
+            error_capacity
+        )
+    ) {
+        free(command_line);
+        fclose(stderr_file);
+        fclose(stdout_file);
+        return false;
+    }
+
+    SECURITY_ATTRIBUTES attributes = {
+        .nLength = sizeof(attributes),
+        .lpSecurityDescriptor = NULL,
+        .bInheritHandle = TRUE,
+    };
+
+    HANDLE stdin_handle = CreateFileA(
+        "NUL",
+        GENERIC_READ,
+        FILE_SHARE_READ | FILE_SHARE_WRITE,
+        &attributes,
+        OPEN_EXISTING,
+        FILE_ATTRIBUTE_NORMAL,
+        NULL
     );
-    return false;
+
+    if (stdin_handle == INVALID_HANDLE_VALUE) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to open process stdin (Win32 error %lu)",
+            (unsigned long)GetLastError()
+        );
+        free(command_line);
+        fclose(stderr_file);
+        fclose(stdout_file);
+        return false;
+    }
+
+    STARTUPINFOA startup = {0};
+    startup.cb = sizeof(startup);
+    startup.dwFlags = STARTF_USESTDHANDLES;
+    startup.hStdInput = stdin_handle;
+    startup.hStdOutput = stdout_handle;
+    startup.hStdError = stderr_handle;
+
+    PROCESS_INFORMATION process = {0};
+
+    BOOL started = CreateProcessA(
+        NULL,
+        command_line,
+        NULL,
+        NULL,
+        TRUE,
+        0,
+        NULL,
+        NULL,
+        &startup,
+        &process
+    );
+
+    DWORD start_error =
+        started
+        ? ERROR_SUCCESS
+        : GetLastError();
+
+    CloseHandle(stdin_handle);
+    free(command_line);
+
+    if (!started) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to start process (Win32 error %lu)",
+            (unsigned long)start_error
+        );
+        fclose(stderr_file);
+        fclose(stdout_file);
+        return false;
+    }
+
+    CloseHandle(process.hThread);
+
+    DWORD wait_result =
+        WaitForSingleObject(
+            process.hProcess,
+            INFINITE
+        );
+
+    if (wait_result != WAIT_OBJECT_0) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to wait for process (Win32 error %lu)",
+            (unsigned long)GetLastError()
+        );
+        CloseHandle(process.hProcess);
+        fclose(stderr_file);
+        fclose(stdout_file);
+        return false;
+    }
+
+    DWORD exit_code = 0;
+
+    if (!GetExitCodeProcess(
+        process.hProcess,
+        &exit_code
+    )) {
+        set_error(
+            error,
+            error_capacity,
+            "unable to read process status (Win32 error %lu)",
+            (unsigned long)GetLastError()
+        );
+        CloseHandle(process.hProcess);
+        fclose(stderr_file);
+        fclose(stdout_file);
+        return false;
+    }
+
+    CloseHandle(process.hProcess);
+    result->status = (int)exit_code;
+
+    bool ok =
+        read_capture(
+            stdout_file,
+            &result->stdout_data,
+            &result->stdout_length,
+            error,
+            error_capacity
+        ) &&
+        read_capture(
+            stderr_file,
+            &result->stderr_data,
+            &result->stderr_length,
+            error,
+            error_capacity
+        );
+
+    fclose(stderr_file);
+    fclose(stdout_file);
+
+    if (!ok) {
+        lune_platform_process_result_free(
+            result
+        );
+        return false;
+    }
+
+    return true;
 }
 
 #endif
